221a0178414c8d7726a845b72c9a9bbbb0357938
alsomedud33 <Alsomedude@hotmail.com>
2021-12-06 10:22:04 +0000

    Fixed bug

--- Godot/ASSETS/TitleScreen/Buttons/OptionsButton.gd
+++ Godot/ASSETS/TitleScreen/Buttons/OptionsButton.gd
@@ -21,8 +21,8 @@ func _on_OptionsButton_pressed():
 		get_tree().change_scene(path)
 
 
-func _on_Area2D_area_entered():
+func _on_Area2D_area_entered(area):
 	emit_signal("focus_entered")
 
-func _on_Area2D_area_exited():
+func _on_Area2D_area_exited(area):
 	emit_signal("focus_exited")
