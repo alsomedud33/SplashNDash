2e0f225be25a6cd0d981e4cc88e33d3aab854d2a
alsomedud33 <Alsomedude@hotmail.com>
2021-12-06 10:20:54 +0000

    Added Pause Menu

--- Godot/ASSETS/HUD/Panel.gd
+++ Godot/ASSETS/HUD/Panel.gd
@@ -10,39 +10,43 @@ var current_state = States.RESUME
 onready var getpointer = $Pointer
 
 func _process(delta):
+#	var dec = getpointer.get_overlapping_areas()
 	if get_parent().Paused == true:
-		if Input.is_action_just_pressed("ui_select_1") or Input.is_action_just_pressed("ui_select_2"):
-			var dec = getpointer.get_overlapping_areas()
-			for b in dec:
-				print (b.name)
-				if b.name == "RESUME":
-					b.get_parent().emit_signal("pressed")
-				if b.name == "CHAR":
-					b.get_parent().emit_signal("pressed")
-				if b.name == "Exit":
-					b.get_parent().emit_signal("pressed")
-	match current_state:
-		States.RESUME:
-			Resume()
-			if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
-				current_state = States.EXIT
-			if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
-				current_state = States.CHAR
-		States.CHAR:
-			Char()
-			if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
-				current_state = States.RESUME
-			if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
-				current_state = States.EXIT
-		States.EXIT:
-			Exit()
-			if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
-				current_state = States.CHAR
-			if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
-				current_state = States.RESUME
-	var dec = getpointer.get_overlapping_areas()
-	for b in dec:
-		print (b.name)
+#		if Input.is_action_just_pressed("ui_select_1") or Input.is_action_just_pressed("ui_select_2"):
+#			var dec = getpointer.get_overlapping_areas()
+#			for b in dec:
+#	#			print (b.name)
+#				if b.name == "RESUME":
+#					b.get_parent().emit_signal("pressed")
+#				if b.name == "CHAR":
+#					b.get_parent().emit_signal("pressed")
+#				if b.name == "Exit":
+#					b.get_parent().emit_signal("pressed")
+		match current_state:
+			States.RESUME:
+				Resume()
+				new_input()
+				if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
+					current_state = States.EXIT
+				if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
+					current_state = States.CHAR
+			States.CHAR:
+				Char()
+				new_input()
+				if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
+					current_state = States.RESUME
+				if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
+					current_state = States.EXIT
+			States.EXIT:
+				Exit()
+				new_input()
+				if Input.is_action_just_pressed("up_1") or Input.is_action_just_pressed("up_2"):
+					current_state = States.CHAR
+				if Input.is_action_just_pressed("down_1") or Input.is_action_just_pressed("down_2"):
+					current_state = States.RESUME
+	#	var dec = getpointer.get_overlapping_areas()
+	#	for b in dec:
+	#		print (b.name)
 
 func Resume():
 	getpointer.global_position = Vector2(1529.874,522.5)
@@ -52,3 +56,16 @@ func Char():
 
 func Exit():
 	getpointer.global_position = Vector2(1534.111,838.229)
+
+
+func new_input():
+	if Input.is_action_just_pressed("ui_select_1") or Input.is_action_just_pressed("ui_select_2"):
+		var dec = getpointer.get_overlapping_areas()
+		for b in dec:
+			print (b.name)
+			if b.name == "RESUME":
+				b.get_parent().emit_signal("pressed")
+			if b.name == "CHAR":
+				b.get_parent().emit_signal("pressed")
+			if b.name == "Exit":
+				b.get_parent().emit_signal("pressed")
--- Godot/ASSETS/HUD/Pause.gd
+++ Godot/ASSETS/HUD/Pause.gd
@@ -5,12 +5,25 @@ var Paused = false
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
-		if Paused == true:
-			get_tree().paused = false
-			Paused = false
-			visible = false
-		if Paused == false:
-			get_tree().paused = true
-			Paused = true
-			visible = true
+		match Paused:
+			true:
+				get_tree().paused = false
+				visible = false
+				print ("unpause")
+				Paused = false
+			false:
+				get_tree().paused = true
+				Physics2DServer.set_active(true)
+				visible = true
+				print ("pause")
+				Paused = true
+#		if Paused == true:
+#			get_tree().paused = false
+#			visible = false
+#			print ("unpause")
+#		if Paused == false:
+#			get_tree().paused = true
+#			Paused = true
+#			visible = true
+#			print ("pause")
 #	print (Paused)
