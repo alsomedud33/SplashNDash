96c4db713c1986b0b5ec578e8b018ae1121c5b2b
alsomedud33 <Alsomedude@hotmail.com>
2021-12-06 12:29:45 +0000

    Pause Screen UI
    added focus to pause screen buttons

--- Godot/ASSETS/HUD/Pause Button.gd
+++ Godot/ASSETS/HUD/Pause Button.gd
@@ -2,8 +2,24 @@ extends Button
 
 export var path = ''
 
+onready var label = get_node("Label")
 
 func _on_Button_pressed():
 	if (path != ''):
 		get_tree().paused = false
 		get_tree().change_scene(path)
+
+func _on_CHAR_area_entered(area):
+	emit_signal("focus_entered")
+
+
+func _on_CHAR_area_exited(area):
+	emit_signal("focus_exited")
+
+
+func _on_Char_Select_focus_entered():
+	label.add_color_override("font_color_shadow", Color(1,0.33,0.2,1))
+
+
+func _on_Char_Select_focus_exited():
+	label.add_color_override("font_color_shadow", Color(0.639216, 0, 1))
--- Godot/ASSETS/UI Features/Resume.gd
+++ Godot/ASSETS/UI Features/Resume.gd
@@ -1,5 +1,7 @@
 extends Button
 
+onready var label = get_node("Label")
+
 func _on_Control_pressed():
 		if get_parent().get_parent().Paused == false:
 			get_tree().paused = true
@@ -10,3 +12,19 @@ func _on_Control_pressed():
 			get_tree().paused = false
 			get_parent().get_parent().Paused = false
 			get_parent().get_parent().visible = false
+
+
+func _on_Resume_focus_entered():
+	label.add_color_override("font_color_shadow", Color(1,0.33,0.2,1))
+
+
+func _on_Resume_focus_exited():
+	label.add_color_override("font_color_shadow", Color(0.639216, 0, 1))
+
+
+func _on_RESUME_area_entered(area):
+	emit_signal("focus_entered")
+
+
+func _on_RESUME_area_exited(area):
+	emit_signal("focus_exited")
