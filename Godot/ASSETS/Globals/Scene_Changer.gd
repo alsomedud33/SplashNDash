3d74f076105d1c00517c8496e7f9887fb9638626
alsomedud33 <Alsomedude@hotmail.com>
2022-01-24 00:19:46 +0000

    Added loading bar

--- Godot/ASSETS/Globals/Scene_Changer.gd
+++ Godot/ASSETS/Globals/Scene_Changer.gd
@@ -13,37 +13,32 @@ func _ready():
 
 func goto_scene(path, current_scene):
 	var loader = ResourceLoader.load_interactive(path)
-	
+	var loading_bar = load("res://ASSETS/UI Features/ProgressBar.tscn").instance()
+	get_tree().get_root().call_deferred('add_child',loading_bar)
 	while true:
 		var err = loader.poll()
 		if err == ERR_FILE_EOF:
 			#loading complete
 			var resource = loader.get_resource()
-			#resource.instance().connect("tree_entered", get_tree(), "set_current_scene", [resource.instance()], CONNECT_ONESHOT)
-			#get_tree().get_root().add_child(resource.instance())
-		#	get_tree().get_root().call_deferred("add_child", resource.instance())
 			call_deferred("_deferred_goto_scene", resource, current_scene)
-		#	get_tree().set_current_scene(resource.instance())
-		#	current_scene.queue_free()
+			loading_bar.queue_free()
 			break
 		if err == OK:
 			#stil loading
 			var progress = float(loader.get_stage())/loader.get_stage_count()
+			loading_bar.get_node("AnimationPlayer").get_node("ProgressBar").value = progress * 100
 			print (progress)
+		yield(get_tree(),"idle_frame")
 
 func _deferred_goto_scene(loader, current_scene):
 	# It is now safe to remove the current scene
 	current_scene.free()
-
-	# Load the new scene.
-#    var s = ResourceLoader.load(path)
 
 	# Instance the new scene.
 	current_scene = loader.instance()
 
 	# Add it to the active scene, as child of root.
 	get_tree().get_root().add_child(current_scene)
-
 	# Optionally, to make it compatible with the SceneTree.change_scene() API.
 	get_tree().set_current_scene(current_scene)
 	print(get_tree().get_current_scene().name)
